7b09f9de19caa88a7029adc6f816095671bb46d0
swj <1061891980@qq.com>
2022-07-27 21:56:53 +0800

    无关文件

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { Player } from "@remotion/player";
+// import { MyComp } from "./remotion/MyComp";
+
+// export const App: React.FC = () => {
+//   return (
+//     <Player
+//       component={MyComp}
+//       inputProps={{ text: "World" }}
+//       durationInFrames={120}
+//       compositionWidth={1920}
+//       compositionHeight={1080}
+//       fps={30}
+//       style={{
+//         width: 1280,
+//         height: 720,
+//       }}
+//       controls
+//     />
+//   );
+// };
