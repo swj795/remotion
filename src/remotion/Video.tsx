98627bd67dd6e0f058ffc0befd95f7f639f067c2
swj <1061891980@qq.com>
2022-07-27 21:59:47 +0800

    主要视频渲染

---
+++ src/remotion/Video.tsx
@@ -0,0 +1,101 @@
+
+import { Composition, continueRender, delayRender } from 'remotion'
+import { useState, useEffect } from 'react';
+import { getVideoMetadata } from "@remotion/media-utils";
+import { MyVideo } from './MyVideo'; 
+import { MyAudioVisual } from './MyAudioVisual';
+import { VideoTesting } from './VideoTesting'
+import { MyDataFetch } from './DataFetch';
+import { FadeIn } from './FadeIn';
+import { EasingAnimations } from './EasingAnimation';
+import { FadeInOut } from './FadeInOut';
+import { ColorChange } from './ColorChange';
+
+export const RemotionVideo: React.FC = () => {
+  const [handle] = useState(() => delayRender());
+  const [duration, setDuration] = useState(1);
+ 
+  useEffect(() => {
+    getVideoMetadata(
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    )
+      .then(({ durationInSeconds }) => {
+        setDuration(Math.round(durationInSeconds * 30));
+        continueRender(handle);
+      })
+      .catch((err) => {
+        console.log(`Error fetching metadata: ${err}`);
+      });
+  }, [handle]);
+    return (
+      <>
+        <Composition
+          id="MyVideo"
+          component={MyVideo}
+          durationInFrames={150}
+          fps={30}
+          width={1920}
+          height={1080}
+          defaultProps={{ hello: "world" }}
+        />
+        <Composition
+          id="MyAudioVisual"
+          component={MyAudioVisual}
+          durationInFrames={150}
+          fps={30}
+          width={1920}
+          height={1080}
+          defaultProps={{ hello: "world" }}
+        />
+        <Composition
+          id="MyDataFetch"
+          component={MyDataFetch}
+          durationInFrames={150}
+          fps={30}
+          width={1920}
+          height={1080}
+          defaultProps={{ hello: "world" }}
+        />
+        <Composition
+        id="dynamic-duration"
+        component={VideoTesting}
+        width={1080}
+        height={1080}
+        fps={30}
+        durationInFrames={duration}
+      />
+      <Composition
+        id="fade-in"
+        component={FadeIn}
+        width={1080}
+        height={1080}
+        fps={30}
+        durationInFrames={duration}
+      />
+      <Composition
+        id="fade-in-out"
+        component={FadeInOut}
+        width={1080}
+        height={1080}
+        fps={30}
+        durationInFrames={100}
+      />
+      <Composition
+        id="easing-animation"
+        component={EasingAnimations}
+        width={1080}
+        height={1080}
+        fps={30}
+        durationInFrames={duration}
+      />
+      <Composition
+        id="color-change"
+        component={ColorChange}
+        width={1080}
+        height={1080}
+        fps={30}
+        durationInFrames={200}
+      />
+      </>
+    );
+  };
